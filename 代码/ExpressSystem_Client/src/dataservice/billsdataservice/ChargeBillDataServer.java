3800d4497287499f73e61ccf5fc7673688218e04
Nickeilf <haozi1996@126.com>
2015-11-26 22:41:04 +0800

    暂且上传一下免得冲突

--- 代码/ExpressSystem_Client/src/dataservice/billsdataservice/ChargeBillDataServer.java
+++ 代码/ExpressSystem_Client/src/dataservice/billsdataservice/ChargeBillDataServer.java
@@ -1,8 +1,10 @@
 package dataservice.billsdataservice;
 
+import java.rmi.Remote;
+
 import po.bills.ChargeBill;
 
-public interface ChargeBillDataServer {
+public interface ChargeBillDataServer extends Remote {
 
     public void addBill(ChargeBill bill);
 	
