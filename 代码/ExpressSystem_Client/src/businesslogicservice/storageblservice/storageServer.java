db167945d1000d0e3669391fc15b39b451684d4f
mzc <mzc19960830@163.com>
2015-12-04 19:17:23 +0800

    Storage接口

--- 代码/ExpressSystem_Client/src/businesslogicservice/storageblservice/storageServer.java
+++ 代码/ExpressSystem_Client/src/businesslogicservice/storageblservice/storageServer.java
@@ -1,6 +1,12 @@
 package businesslogicservice.storageblservice;
 
+import java.util.Iterator;
+
+import po.GoodPO;
 import po.Message;
+import po.Institution.StoragePO;
+import po.bills.ExportBill;
+import po.bills.ImportBill;
 import vo.BillVO;
 import vo.StorageVO;
 
@@ -8,18 +14,14 @@ import vo.StorageVO;
 
 public interface storageServer {
 	
-
-	public BillVO makeImportBill(Message message);
+    public boolean ImportGood(StoragePO storage,GoodPO good);//入库，要同时修改货物与仓库的储存信息
+    
+    public boolean ExportGood(StoragePO storage,GoodPO good);//出库，同上
+    
+	public Iterator<GoodPO> getGoodsList(StoragePO storage);//获得当前仓库货物清单
 	
-	public BillVO makeExportBill(Message message);
-	
+	public Iterator getStorageHistory(StoragePO storage,String startTim,String endTime);//获得出入库记录功能
 
-	public StorageVO getGoodsList();
-	
-
-	public Message getStorageHistory(String startTim,String endTime);
-
-	
-	public Message changeStorage(String id,String newLocation);
+	public Iterator changeStorage(StoragePO storage,String GoodId,String newLocation);//分区调整功能
 
 }
