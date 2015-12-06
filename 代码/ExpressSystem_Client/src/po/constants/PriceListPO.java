ed0b3e36ced1195df3681ec598d4ac5047d132e5
Nickeilf <haozi1996@126.com>
2015-12-06 17:05:45 +0800

    1

---
+++ 代码/ExpressSystem_Client/src/po/constants/PriceListPO.java
@@ -0,0 +1,90 @@
+package po.constants;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+
+/**
+ * 各种各样的价格列表
+ * @author rabook
+ *
+ */
+public class PriceListPO implements Serializable,Remote{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5429360673004104582L;
+
+	private double CarPrice;
+	
+	private double TrainPrice;
+	
+	private double AirPrice;
+	
+	private double standardPrice;
+	
+	private double economicPrice;
+
+	private double expressPrice;
+	
+	public PriceListPO(){
+		this.CarPrice=2;
+		this.TrainPrice=0.2;
+		this.AirPrice=20;
+		this.standardPrice=0.023;
+		this.expressPrice=0.025;
+		this.economicPrice=0.018;
+	}
+	
+	
+	//Getter and Setter
+	public double getEconomicPrice() {
+		return economicPrice;
+	}
+
+
+	public void setEconomicPrice(double economicPrice) {
+		this.economicPrice = economicPrice;
+	}
+
+
+	public double getExpressPrice() {
+		return expressPrice;
+	}
+
+
+	public void setExpressPrice(double expressPrice) {
+		this.expressPrice = expressPrice;
+	}
+	public double getCarPrice() {
+		return CarPrice;
+	}
+
+	public void setCarPrice(double carPrice) {
+		CarPrice = carPrice;
+	}
+
+	public double getTrainPrice() {
+		return TrainPrice;
+	}
+
+	public void setTrainPrice(double trainPrice) {
+		TrainPrice = trainPrice;
+	}
+
+	public double getAirPrice() {
+		return AirPrice;
+	}
+
+	public void setAirPrice(double airPrice) {
+		AirPrice = airPrice;
+	}
+
+	public double getStandardPrice() {
+		return standardPrice;
+	}
+
+	public void setStandardPrice(double standardPrice) {
+		this.standardPrice = standardPrice;
+	}
+}
---
+++ 代码/ExpressSystem_Server/src/po/constants/CityDistancePO.java
@@ -0,0 +1,46 @@
+package po.constants;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+
+public class CityDistancePO implements Serializable,Remote{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7457460054259364116L;
+	String city1;
+	String city2;
+	
+	double distance;
+	
+	public CityDistancePO(String city1,String city2,double distance){
+		this.city1=city1;
+		this.city2=city2;
+		this.distance=distance;
+		
+	}
+	
+	public double getDistance(){
+		return distance;
+	}
+	
+	public void setDistance(String distance){
+		this.distance=Double.valueOf(distance);
+	}
+	
+	/**
+	 * 这个方法通过输入的城市判断是否是指的该对象
+	 * 城市前后不影响结果
+	 * @param city1
+	 * @param city2
+	 * @return
+	 */
+	public boolean isThis(String city1,String city2){
+		if((this.city1.equals(city1)&&this.city2.equals(city2))
+				||(this.city1.equals(city2)&&this.city2.equals(city1))){
+			return true;
+		}else
+			return false;
+	}
+}
