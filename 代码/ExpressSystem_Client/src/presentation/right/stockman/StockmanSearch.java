4a8d8f7f2911415227ee11f7991c7c925bec79f7
Lemonstars <1078595232@qq.com>
2015-12-06 17:10:08 +0800

    库存查看

--- 代码/ExpressSystem_Client/src/presentation/right/stockman/StockmanSearch.java
+++ 代码/ExpressSystem_Client/src/presentation/right/stockman/StockmanSearch.java
@@ -6,18 +6,34 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 import presentation.right.RightAll;
 import presentation.watcher.*;
 
-public class StockmanSearch extends RightAll implements ActionListener{
+public class StockmanSearch extends RightAll implements ActionListener {
 	int frameWidth;
 	int frameHeight;
 	JLabel[] jl;
 	JButton search;
 	private List<Watcher> list;
+
+	JLabel time[];
+	JComboBox<String>[] timeInput;
+	JLabel timeover[];
+	JComboBox<String>[] timeInputover;
+
+	JPanel addpanel;
+	DefaultTableModel tableModel;
+	JTable table;
+	JScrollPane js;
+	JLabel inStock;
+	JLabel outStock;
+	JTextField jtfIn;
+	JTextField jtfOut;
 
 	public StockmanSearch(int frameWidth, int frameHeight) {
 
@@ -27,21 +43,48 @@ public class StockmanSearch extends RightAll implements ActionListener{
 		list = new ArrayList<Watcher>();
 
 		this.setLayout(null);
-		this.setBackground(new Color(254, 67, 101));
 		this.setBounds(frameWidth / 4, 0, frameWidth * 3 / 4, frameHeight);
 
 		jl = new JLabel[2];
 		for (int i = 0; i < 2; i++) {
 			jl[i] = new JLabel();
 		}
-		search=new JButton("查询");
+		search = new JButton("查询");
+
+		time = new JLabel[3];
+		timeInput = new JComboBox[3];
+		timeover = new JLabel[3];
+		timeInputover = new JComboBox[3];
+		for (int i = 0; i < 3; i++) {
+			time[i] = new JLabel();
+			timeover[i] = new JLabel();
+		}
+		String[] year = { "2015", "2016", "2017", "2018", "2019", "2020" };
+		String[] month = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+				"11", "12" };
+		String[] day = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+				"11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+				"21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+				"31" };
+		timeInput[0] = new JComboBox<String>(year);
+		timeInput[1] = new JComboBox<String>(month);
+		timeInput[2] = new JComboBox<String>(day);
+		timeInputover[0] = new JComboBox<String>(year);
+		timeInputover[1] = new JComboBox<String>(month);
+		timeInputover[2] = new JComboBox<String>(day);
 
 		init();
 
-		for (int i = 0; i <2; i++) {
+		for (int i = 0; i < 2; i++) {
 			this.add(jl[i]);
 		}
 		this.add(search);
+		for (int i = 0; i < 3; i++) {
+			this.add(time[i]);
+			this.add(timeInput[i]);
+			this.add(timeover[i]);
+			this.add(timeInputover[i]);
+		}
 
 	}
 
@@ -49,13 +92,92 @@ public class StockmanSearch extends RightAll implements ActionListener{
 
 		jl[0].setText("起始时间");
 		jl[1].setText("中止时间 ");
-		 
-		for (int i = 0; i < 2; i++) {
-			jl[i].setBounds(frameWidth / 8+frameWidth/3*i, frameHeight / 10 , 100, 65);
-		}
-		 
+		jl[0].setBounds(frameWidth / 6, frameHeight / 10, frameWidth / 10,
+				frameHeight / 20);
+		jl[1].setBounds(frameWidth / 2 + frameWidth / 10, frameHeight / 10,
+				frameWidth / 10, frameHeight / 20);
+		search.setBounds(frameWidth / 4, frameHeight / 4, frameWidth / 10,
+				frameHeight / 20);
+		search.addActionListener(this);
 
-		search.setBounds( frameWidth/4,frameHeight/5,80,30);
+		time[0].setText("年");
+		time[1].setText("月");
+		time[2].setText("日");
+		for (int i = 0; i < 3; i++) {
+			timeInput[i].setBounds(frameWidth / 12 + frameWidth / 10 * i,
+					frameHeight / 20 + frameHeight / 10, frameWidth / 12,
+					frameHeight / 20);
+			time[i].setBounds(frameWidth / 6 + frameWidth / 10 * i, frameHeight
+					/ 20 + frameHeight / 10, frameWidth / 12, frameHeight / 20);
+		}
+
+		timeover[0].setText("年");
+		timeover[1].setText("月");
+		timeover[2].setText("日");
+		for (int i = 0; i < 3; i++) {
+			timeInputover[i].setBounds(frameWidth / 12 * 5 + frameWidth / 10
+					* i, frameHeight / 20 + frameHeight / 10, frameWidth / 12,
+					frameHeight / 20);
+			timeover[i].setBounds(frameWidth / 2 + frameWidth / 10 * i,
+					frameHeight / 20 + frameHeight / 10, frameWidth / 12,
+					frameHeight / 20);
+		}
+	}
+
+	private void addPanel() {
+		addpanel = new JPanel();
+		addpanel.setLayout(null);
+		addpanel.setBounds(0, frameHeight / 3, frameWidth / 4 * 3,
+				frameHeight / 3 * 2);
+
+		tableModel = new DefaultTableModel();
+		table = new JTable(tableModel);
+		js = new JScrollPane(table);
+		inStock = new JLabel("入库合计");
+		outStock = new JLabel("出库合计");
+		jtfIn = new JTextField();
+		jtfOut = new JTextField();
+
+		js.setBounds(0, 0, frameWidth / 4 * 3, frameHeight / 2);
+		inStock.setBounds(0, frameHeight / 2, frameWidth / 10, frameHeight / 20);
+		outStock.setBounds(0, frameHeight / 2 + frameHeight / 10,
+				frameWidth / 10, frameHeight / 20);
+		jtfIn.setBounds(frameWidth / 10, frameHeight / 2, frameWidth / 10,
+				frameHeight / 20);
+		jtfOut.setBounds(frameWidth / 10, frameHeight / 2 + frameHeight / 10,
+				frameWidth / 10, frameHeight / 20);
+
+		initTable();
+
+		addpanel.add(jtfIn);
+		addpanel.add(jtfOut);
+		addpanel.add(js);
+		addpanel.add(inStock);
+		addpanel.add(outStock);
+		this.add(addpanel);
+		this.repaint();
+	}
+
+	private void initTable() {
+		tableModel.addColumn("快递编号");
+		tableModel.addColumn("出/入库");
+		tableModel.addColumn("金额");
+		tableModel.addColumn("库存位置");
+		table.getTableHeader().setReorderingAllowed(false);
+		table.getTableHeader().setResizingAllowed(false);
+
+		initTableModel();
+	}
+
+	private void initTableModel() {
+		Vector<String> vec = new Vector<>();
+
+		vec.add("110232124");
+		vec.add("入库");
+		vec.add("1000");
+		vec.add("04区01排01架01号");
+
+		tableModel.addRow(vec);
 	}
 
 	public void addWatcher(Watcher watcher) {
@@ -74,7 +196,7 @@ public class StockmanSearch extends RightAll implements ActionListener{
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == search) {
-			//this.notifyWatchers(State.STOCKMANSTART);
-		} 
+			addPanel();
+		}
 	}
 }
--- 代码/ExpressSystem_Client/src/presentation/right/ying_salesman/Ying_manageInfor.java
+++ 代码/ExpressSystem_Client/src/presentation/right/ying_salesman/Ying_manageInfor.java
@@ -90,13 +90,13 @@ public class Ying_manageInfor extends RightAll implements ActionListener {
 
 			// 司机管理
 			if (selectedItem.equals("司机")) {
-				System.out.println("driver");
 				addpanel = new DriverPanel();
-				this.add(addpanel);
-				this.repaint();
 			} else if (selectedItem.equals("车辆")) {
-
+				addpanel = new CarPanel();
 			}
+
+			this.add(addpanel);
+			this.repaint();
 
 		}
 
@@ -281,9 +281,19 @@ public class Ying_manageInfor extends RightAll implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 
 			if (e.getSource() == jb[0]) {
+				if (changepanel != null) {
+					this.remove(changepanel);
+					this.repaint();
+				}
+
 				// 增加
 				addAddPanel();
 			} else if (e.getSource() == jb[1]) {
+				if (addpanel != null) {
+					this.remove(addpanel);
+					this.repaint();
+				}
+
 				// 删
 				int selectedRow = table.getSelectedRow();
 				if (selectedRow >= 0) {
@@ -291,11 +301,18 @@ public class Ying_manageInfor extends RightAll implements ActionListener {
 				}
 
 			} else if (e.getSource() == jb[2]) {
+				if (addpanel != null) {
+					this.remove(addpanel);
+					this.repaint();
+				}
+				if (changepanel != null) {
+					this.remove(changepanel);
+					this.repaint();
+				}
+
 				// 改
 				int row = table.getSelectedRow();
 				if (row >= 0) {
-					tableModel.removeRow(row);
-
 					Vector<String> vec = new Vector<String>();
 
 					vec.add((String) (table.getValueAt(row, 0)));
@@ -307,6 +324,262 @@ public class Ying_manageInfor extends RightAll implements ActionListener {
 					vec.add((String) (table.getValueAt(row, 6)));
 
 					addChangePanel(vec);
+					tableModel.removeRow(row);
+
+				}
+
+			}
+
+			// 增加成功
+			if (e.getSource() == addover) {
+				this.remove(addpanel);
+				this.repaint();
+
+				Vector<String> vec = new Vector<>();
+				for (int i = 0; i < 7; i++) {
+					vec.add(addjtf[i].getText());
+				}
+				tableModel.addRow(vec);
+			}
+
+			if (e.getSource() == changeover) {
+				this.remove(changepanel);
+				this.repaint();
+
+				Vector<String> vec = new Vector<>();
+				for (int i = 0; i < 7; i++) {
+					vec.add(changejtf[i].getText());
+				}
+				tableModel.addRow(vec);
+			}
+
+		}
+	}
+
+	class CarPanel extends JPanel implements ActionListener {
+
+		DefaultTableModel tableModel;
+		JTable table;
+		JScrollPane js;
+		JButton jb[];
+
+		JPanel addpanel;
+		JTextField addjtf[];
+		JLabel addlable[];
+		JButton addover;
+
+		JPanel changepanel;
+		JTextField changejtf[];
+		JLabel changelable[];
+		JButton changeover;
+
+		public CarPanel() {
+			this.setBounds(0, frameHeight / 5, frameWidth / 4 * 3,
+					frameHeight / 5 * 4);
+			this.setLayout(null);
+			this.setBackground(Color.gray);
+
+			tableModel = new DefaultTableModel();
+			table = new JTable(tableModel);
+			js = new JScrollPane(table);
+
+			jb = new JButton[3];
+			for (int i = 0; i < 3; i++) {
+				jb[i] = new JButton();
+			}
+
+			initThis();
+
+			this.add(js);
+			for (int i = 0; i < 3; i++) {
+				this.add(jb[i]);
+			}
+		}
+
+		private void initThis() {
+			js.setBounds(0, 0, frameWidth / 4 * 3, frameHeight / 5 * 3);
+			initThisTable();
+
+			jb[0].setText("增加");
+			jb[1].setText("删除");
+			jb[2].setText("修改");
+			for (int i = 0; i < 3; i++) {
+				jb[i].setBounds(frameWidth / 8 + frameWidth / 5 * i,
+						frameHeight / 4 * 3, frameWidth / 10, frameHeight / 20);
+				jb[i].addActionListener(this);
+			}
+		}
+
+		// 初始化table
+		private void initThisTable() {
+			tableModel.addColumn("车辆代号");
+			tableModel.addColumn("发动机号");
+			tableModel.addColumn("车辆号");
+			tableModel.addColumn("底盘号");
+			tableModel.addColumn("购买时间");
+			tableModel.addColumn("服役时间");
+			tableModel.addColumn("车辆描述 ");
+			table.getColumnModel().getColumn(0)
+					.setPreferredWidth(frameWidth / 30);
+			table.getColumnModel().getColumn(1)
+					.setPreferredWidth(frameWidth / 30);
+			table.getColumnModel().getColumn(2)
+					.setPreferredWidth(frameWidth / 30);
+			table.getColumnModel().getColumn(3)
+					.setPreferredWidth(frameWidth / 10);
+			table.getColumnModel().getColumn(4)
+					.setPreferredWidth(frameWidth / 30);
+			table.getColumnModel().getColumn(5)
+					.setPreferredWidth(frameWidth / 30);
+			table.getTableHeader().setReorderingAllowed(false);
+			table.getTableHeader().setResizingAllowed(false);
+
+			initThisTableModel();
+
+		}
+
+		// 初始化model,@ma
+		private void initThisTableModel() {
+			Vector<String> vec = new Vector<>();
+			vec.add("陈信宏");
+			vec.add("141250079");
+			vec.add("1993.12.12");
+			vec.add("321302199512181111");
+			vec.add("18761681111");
+			vec.add("男");
+			vec.add("2015.1.1—2020.12.31");
+
+			tableModel.addRow(vec);
+		}
+
+		private void addAddPanel() {
+			addpanel = new JPanel();
+			addpanel.setLayout(null);
+			addpanel.setBounds(0, frameHeight / 5 * 3, frameWidth / 4 * 3,
+					frameHeight / 10);
+			addlable = new JLabel[7];
+			addjtf = new JTextField[7];
+			for (int i = 0; i < 7; i++) {
+				addlable[i] = new JLabel();
+				addjtf[i] = new JTextField();
+			}
+			addover = new JButton("√");
+			addlable[0].setText("车辆代号");
+			addlable[1].setText("发动机号");
+			addlable[2].setText("车辆号");
+			addlable[3].setText("底盘号");
+			addlable[4].setText("购买时间");
+			addlable[5].setText("服役时间");
+			addlable[6].setText("车辆描述");
+
+			for (int i = 0; i < 7; i++) {
+				addlable[i].setBounds(frameWidth / 10 * i, 0,
+						frameWidth / 28 * 3, frameHeight / 20);
+				addjtf[i].setBounds(frameWidth / 10 * i, frameHeight / 20,
+						frameWidth / 28 * 3, frameHeight / 20);
+
+				addpanel.add(addlable[i]);
+				addpanel.add(addjtf[i]);
+			}
+			addover.setBounds(frameWidth / 10 * 7, frameHeight / 20,
+					frameWidth / 20, frameHeight / 20);
+			addover.addActionListener(this);
+
+			addpanel.add(addover);
+			this.add(addpanel);
+			this.repaint();
+
+		}
+
+		private void addChangePanel(Vector<String> vec) {
+			changepanel = new JPanel();
+			changepanel.setLayout(null);
+			changepanel.setBounds(0, frameHeight / 5 * 3, frameWidth / 4 * 3,
+					frameHeight / 10);
+			changelable = new JLabel[7];
+			changejtf = new JTextField[7];
+			for (int i = 0; i < 7; i++) {
+				changelable[i] = new JLabel();
+				changejtf[i] = new JTextField();
+			}
+			changeover = new JButton("√");
+			changelable[0].setText("车辆代号");
+			changelable[1].setText("发动机号");
+			changelable[2].setText("车辆号");
+			changelable[3].setText("底盘号");
+			changelable[4].setText("购买时间");
+			changelable[5].setText("服役时间");
+			changelable[6].setText("车辆描述");
+
+			for (int i = 0; i < 7; i++) {
+				changelable[i].setBounds(frameWidth / 10 * i, 0,
+						frameWidth / 28 * 3, frameHeight / 20);
+				changejtf[i].setBounds(frameWidth / 10 * i, frameHeight / 20,
+						frameWidth / 28 * 3, frameHeight / 20);
+
+				changejtf[i].setText(vec.get(i));
+
+				changepanel.add(changelable[i]);
+				changepanel.add(changejtf[i]);
+			}
+			changeover.setBounds(frameWidth / 10 * 7, frameHeight / 20,
+					frameWidth / 20, frameHeight / 20);
+			changeover.addActionListener(this);
+
+			changepanel.add(changeover);
+			this.add(changepanel);
+			this.repaint();
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			if (e.getSource() == jb[0]) {
+				if (changepanel != null) {
+					this.remove(changepanel);
+					this.repaint();
+				}
+
+				// 增加
+				addAddPanel();
+			} else if (e.getSource() == jb[1]) {
+				if (addpanel != null) {
+					this.remove(addpanel);
+					this.repaint();
+				}
+
+				// 删
+				int selectedRow = table.getSelectedRow();
+				if (selectedRow >= 0) {
+					tableModel.removeRow(selectedRow);
+				}
+
+			} else if (e.getSource() == jb[2]) {
+				if (addpanel != null) {
+					this.remove(addpanel);
+					this.repaint();
+				}
+				if (changepanel != null) {
+					this.remove(changepanel);
+					this.repaint();
+				}
+
+				// 改
+				int row = table.getSelectedRow();
+				if (row >= 0) {
+					Vector<String> vec = new Vector<String>();
+
+					vec.add((String) (table.getValueAt(row, 0)));
+					vec.add((String) (table.getValueAt(row, 1)));
+					vec.add((String) (table.getValueAt(row, 2)));
+					vec.add((String) (table.getValueAt(row, 3)));
+					vec.add((String) (table.getValueAt(row, 4)));
+					vec.add((String) (table.getValueAt(row, 5)));
+					vec.add((String) (table.getValueAt(row, 6)));
+
+					addChangePanel(vec);
+					tableModel.removeRow(row);
+
 				}
 
 			}
