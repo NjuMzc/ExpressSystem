639272dfe7ab388af91136dbf672d2e97ec46402
Nickeilf <haozi1996@126.com>
2015-12-07 19:22:40 +0800

    1222

--- 代码/ExpressSystem_Server/src/dataservice/billsdataservice/ExportBillDataServer.java
+++ 代码/ExpressSystem_Server/src/dataservice/billsdataservice/ExportBillDataServer.java
@@ -1,12 +1,15 @@
 package dataservice.billsdataservice;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 import po.bills.ExportBill;
 
-public interface ExportBillDataServer {
-    public void addBill(ExportBill bill);
+public interface ExportBillDataServer extends Remote{
+    public void addBill(ExportBill bill)throws RemoteException;
 	
-	public boolean removeBill(String id);
+	public boolean removeBill(String id)throws RemoteException;
 	
-	public ExportBill findBill(String id);
+	public ExportBill findBill(String id)throws RemoteException;
 	
 }
