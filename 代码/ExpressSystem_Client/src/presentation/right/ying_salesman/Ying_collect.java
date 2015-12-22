98cfd3ba907c80772a7e0b09426d95158c3d55f5
Lemonstars <1078595232@qq.com>
2015-12-22 21:41:13 +0800

    ==

--- 代码/ExpressSystem_Client/src/presentation/right/ying_salesman/Ying_collect.java
+++ 代码/ExpressSystem_Client/src/presentation/right/ying_salesman/Ying_collect.java
@@ -14,6 +14,7 @@ import businesslogic.transportbl.hallStaff.Trans_HallSendingServerImpl;
 import businesslogicservice.transportblservice.hallStaff.Trans_HallSendingServer;
 import po.bills.SendingBill;
 import presentation.right.RightAll;
+import presentation.right.YearMonthDay;
 import presentation.watcher.*;
 
 public class Ying_collect extends RightAll implements ActionListener {
@@ -25,12 +26,14 @@ public class Ying_collect extends RightAll implements ActionListener {
 	JTextField jtf[];
 	JButton confirm;
 	JButton cancel;
+	JComboBox<String>[] timeInput;
+	JLabel time[];
 
 	private List<Watcher> list;
 
 	public Ying_collect(int frameWidth, int frameHeight) {
-		blServer=new Trans_HallSendingServerImpl();
-		
+		blServer = new Trans_HallSendingServerImpl();
+
 		this.frameWidth = frameWidth;
 		this.frameHeight = frameHeight;
 
@@ -43,13 +46,22 @@ public class Ying_collect extends RightAll implements ActionListener {
 		for (int i = 0; i < 4; i++) {
 			jl[i] = new JLabel();
 		}
-		confirm = new JButton("");//确认
-		cancel = new JButton("");//取消
-		jtf = new JTextField[3];
-		for (int i = 0; i < 3; i++) {
+		confirm = new JButton("");// 确认
+		cancel = new JButton("");// 取消
+		jtf = new JTextField[2];
+		for (int i = 0; i < 2; i++) {
 			jtf[i] = new JTextField();
 		}
-  
+
+		timeInput = new JComboBox[3];
+		time = new JLabel[3];
+		for (int i = 0; i < 3; i++) {
+			time[i] = new JLabel();
+		}
+		YearMonthDay time1 = new YearMonthDay();
+		timeInput[0] = time1.getCboYear();
+		timeInput[1] = time1.getCboMonth();
+		timeInput[2] = time1.getCboDay();
 
 		init();
 
@@ -58,59 +70,79 @@ public class Ying_collect extends RightAll implements ActionListener {
 		}
 		this.add(confirm);
 		this.add(cancel);
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 2; i++) {
 			this.add(jtf[i]);
 		}
+		for (int i = 0; i < 3; i++) {
+			this.add(time[i]);
+			this.add(timeInput[i]);
+		}
 	}
-	
+
 	protected void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paintComponent(g);
 		ImageIcon background = new ImageIcon("pictures\\派件单right.png");
 		Image bg = background.getImage();
-		g.drawImage(bg, 0, 0, frameWidth/4*3,frameHeight,null);
+		g.drawImage(bg, 0, 0, frameWidth / 4 * 3, frameHeight, null);
 	}
-	
+
 	private void init() {
 		jl[0].setText("派件单");
 		jl[1].setText("到达日期");
 		jl[2].setText("托运单条形码");
 		jl[3].setText("快递员");
 
-		jl[0].setBounds(frameWidth /3, frameHeight / 10, frameWidth /5,
+		jl[0].setBounds(frameWidth / 3, frameHeight / 10, frameWidth / 5,
 				frameHeight / 20);
-		jl[0].setFont(new Font("黑体",Font.BOLD,22));
+		jl[0].setFont(new Font("黑体", Font.BOLD, 22));
 		for (int i = 1; i < 4; i++) {
-			jl[i].setBounds(frameWidth / 10, frameHeight / 36*5*i + frameHeight/11+frameHeight/80,
-					frameWidth / 8, frameHeight / 20);
-			jl[i].setFont(new Font("宋体",Font.BOLD,17));
+			jl[i].setBounds(frameWidth / 10, frameHeight / 36 * 5 * i
+					+ frameHeight / 11 + frameHeight / 80, frameWidth / 8,
+					frameHeight / 20);
+			jl[i].setFont(new Font("宋体", Font.BOLD, 17));
 		}
 
-		confirm.setBounds(frameWidth / 6, frameHeight * 8 / 10,
-				 frameWidth / 9,frameHeight / 16);
+		confirm.setBounds(frameWidth / 6, frameHeight * 8 / 10, frameWidth / 9,
+				frameHeight / 16);
 		confirm.addActionListener(this);
-		cancel.setBounds(frameWidth * 2 / 5+frameWidth/15, frameHeight * 8 / 10,
-				 frameWidth / 9,frameHeight / 16);
+		cancel.setBounds(frameWidth * 2 / 5 + frameWidth / 15,
+				frameHeight * 8 / 10, frameWidth / 9, frameHeight / 16);
 		cancel.addActionListener(this);
-		
+
+		time[0].setText("年");
+		time[1].setText("月");
+		time[2].setText("日");
+		for (int i = 0; i < 3; i++) {
+			timeInput[i].setBounds(frameWidth / 4 + frameWidth / 10 * i,
+					frameHeight / 36 * 5 + frameHeight / 11 + frameHeight / 80,
+					frameWidth / 12, frameHeight / 20);
+			time[i].setBounds(frameWidth / 3 + frameWidth / 10 * i, frameHeight
+					/ 36 * 5 + frameHeight / 11 + frameHeight / 80,
+					frameWidth / 12, frameHeight / 20);
+			time[i].setFont(new Font("宋体", Font.PLAIN, 14));
+			timeInput[i].setFont(new Font("宋体", Font.PLAIN, 14));
+		}
+
 		ImageIcon icon1 = new ImageIcon("pictures//取消t.png");
 		Image temp1 = icon1.getImage().getScaledInstance(icon1.getIconWidth(),
 				icon1.getIconHeight(), icon1.getImage().SCALE_DEFAULT);
 		icon1 = new ImageIcon(temp1);
 		cancel.setIcon(icon1);
-		
+
 		ImageIcon icon2 = new ImageIcon("pictures//确认小.png");
 		Image temp2 = icon2.getImage().getScaledInstance(icon2.getIconWidth(),
 				icon2.getIconHeight(), icon2.getImage().SCALE_DEFAULT);
 		icon2 = new ImageIcon(temp2);
 		confirm.setIcon(icon2);
 
-		for (int i = 0; i < 3; i++) {
-			jtf[i].setBounds(frameWidth / 4, frameHeight / 36*5* (1 + i)+frameHeight/11+frameHeight/80,
-					frameWidth / 8, frameHeight / 18);
-			jtf[i].setFont(new Font("宋体",Font.PLAIN,15));
+		for (int i = 0; i < 2; i++) {
+			jtf[i].setBounds(frameWidth / 4, frameHeight / 36 * 5 * (2 + i)
+					+ frameHeight / 11 + frameHeight / 80, frameWidth / 8,
+					frameHeight / 18);
+			jtf[i].setFont(new Font("宋体", Font.PLAIN, 15));
 		}
-		
+
 		jtf[0].addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER
@@ -119,24 +151,10 @@ public class Ying_collect extends RightAll implements ActionListener {
 				}
 			}
 		});
+
 		jtf[1].addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_ENTER
-						|| e.getKeyCode() == KeyEvent.VK_DOWN) {
-					jtf[2].requestFocus();
-				}
-			}
-		});
-		jtf[2].addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e) {
-				  if (e.getKeyCode() == KeyEvent.VK_UP) {
-					jtf[1].requestFocus();
-				}
-			}
-		});
-		jtf[1].addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e) {
-				  if (e.getKeyCode() == KeyEvent.VK_UP) {
+				if (e.getKeyCode() == KeyEvent.VK_UP) {
 					jtf[0].requestFocus();
 				}
 			}
@@ -161,19 +179,18 @@ public class Ying_collect extends RightAll implements ActionListener {
 		if (e.getSource() == cancel) {
 			this.notifyWatchers(State.ZHONG_START);
 		} else if (e.getSource() == confirm) {
-			String date="";
-//			String year=timeInput[0].getSelectedItem().toString();
-//			String month=timeInput[1].getSelectedItem().toString();
-//			String day=timeInput[2].getSelectedItem().toString();
-//			
-//			date=year+"-"+month+"-"+day;
-			String orderId=jtf[1].getText();
-			String sender=jtf[2].getText();
-			
-			SendingBill bill=blServer.makeBill(date, orderId, sender);
-			
-		 
+			String date = "";
+			// String year=timeInput[0].getSelectedItem().toString();
+			// String month=timeInput[1].getSelectedItem().toString();
+			// String day=timeInput[2].getSelectedItem().toString();
+			//
+			// date=year+"-"+month+"-"+day;
+			String orderId = jtf[0].getText();
+			String sender = jtf[1].getText();
+
+			SendingBill bill = blServer.makeBill(date, orderId, sender);
+
 		}
- 
+
 	}
 }
