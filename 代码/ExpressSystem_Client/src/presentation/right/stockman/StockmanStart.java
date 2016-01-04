badce27556f09da181307d6e30c5a965419a747a
Lemonstars <1078595232@qq.com>
2016-01-04 19:40:02 +0800

    报警

--- 代码/ExpressSystem_Client/src/presentation/right/stockman/StockmanStart.java
+++ 代码/ExpressSystem_Client/src/presentation/right/stockman/StockmanStart.java
@@ -1,6 +1,7 @@
 package presentation.right.stockman;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 
@@ -11,10 +12,11 @@ import presentation.watcher.State;
 import presentation.watcher.Watched;
 import presentation.watcher.Watcher;
 
-public class StockmanStart extends RightAll   {
+public class StockmanStart extends RightAll {
 	int frameWidth;
 	int frameHeight;
 	JLabel jl;
+	boolean isLimited = true;
 
 	public StockmanStart(int frameWidth, int frameHeight) {
 
@@ -24,44 +26,46 @@ public class StockmanStart extends RightAll   {
 		this.setLayout(null);
 		this.setBounds(frameWidth / 4, 0, frameWidth * 3 / 4, frameHeight);
 
-	//	jl = new JLabel("start");
+		jl = new JLabel("start");
 
 		init();
 
 		this.setBackground(new Color(254, 67, 101));
-	//	this.add(jl);
+		this.add(jl);
 
 	}
-	
+
 	protected void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paintComponent(g);
 		ImageIcon background = new ImageIcon("pictures\\系统管理startRight.png");
-		Image bg =background.getImage();
-		g.drawImage(bg, 0, 0,frameWidth*3/4,frameHeight,null);
+		Image bg = background.getImage();
+		g.drawImage(bg, 0, 0, frameWidth * 3 / 4, frameHeight, null);
 	}
-	
 
 	private void init() {
-//		jl.setBounds(320, 320, 300, 300);
-//		jl.setText("start");
+		jl.setBounds(320, 320, 300, 300);
+		if (isLimited) {
+			jl.setFont(new Font("宋体", Font.BOLD, 20));
+			jl.setText("库存报警");
+		}
 	}
 
 	@Override
 	public void addWatcher(Watcher watcher) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void removeWatcehr(Watcher watcher) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void notifyWatchers(State state) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
